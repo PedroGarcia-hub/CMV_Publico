621a60f839b382c606a1dbe344b93169407d21e2
PedroGarcia <76249433+Tuyk@users.noreply.github.com>
2021-06-04 12:48:35 +0200

    Carousel XML
    Carousel XML

--- 04062021-CarouselXML/script.js
+++ 04062021-CarouselXML/script.js
@@ -1,5 +1,5 @@
 let loaded = [];
-let content;
+let content = "";
 
 function readXML(){
     var xhr = new XMLHttpRequest();
@@ -20,12 +20,8 @@ function loadContent(xml){
     var element = [];
 
     var x = xmlDoc.getElementsByTagName("elemento");
-
-    content = `
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-    `
-    for(i = 0; i < x.length; i++){
+    
+    for(i = 0; i <x.length; i++){
         name = x[i].getElementsByTagName('nombre')[0].childNodes[0].nodeValue;
         photo = x[i].getElementsByTagName('foto')[0].childNodes[0].nodeValue;
         foot = x[i].getElementsByTagName('pie')[0].childNodes[0].nodeValue;
@@ -37,35 +33,15 @@ function loadContent(xml){
                         <p>${foot}</p>
                     </div>
         `
-        //content += '<div class = "carousel-item active"><p>'+name+'</p><img class = "d-block w-100" src="'+photo+'"><p>'+foot+'</p></div>';
+        
         element = [name, photo, foot];
         loaded.push(element);
     }
     content += `
-                </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
+        </div>
+
     `
-    document.getElementById('toInsert').innerHTML += content;
+    document.getElementById('items').innerHTML = content;
 }
 
-/**
- * <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-        </div>
- */
